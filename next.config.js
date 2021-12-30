38e8fb5054ab9c6bccfbf5b50291ec4427f13e20
Lukas Cornille <lukascornille@hotmail.com>
2021-12-30 14:01:42 +0100

    refactor: move assets to dedicated folder

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ const { join } = require('path');
 const { createRemarkPlugin } = require('@atomiks/mdx-pretty-code');
 const fs = require('fs');
 
+const getTheme = (path) => JSON.parse(fs.readFileSync(require.resolve(path), 'utf-8'));
+
 const prettyCode = createRemarkPlugin({
-  shikiOptions: {
-    theme: JSON.parse(fs.readFileSync(require.resolve('./src/assets/OneDark-Pro.json'), 'utf-8')),
-  },
+  shikiOptions: { theme: getTheme('./src/assets/themes/OneDark-Pro.json') },
 });
 
 const withMDX = require('@next/mdx')({
@@ -24,5 +24,14 @@ module.exports = withMDX({
   basePath: process.env.BASE_PATH,
   pageExtensions: ['tsx', 'mdx'],
   reactStrictMode: true,
-  sassOptions: { includePaths: [join(__dirname, 'src/scss')] },
+  sassOptions: { includePaths: [join(__dirname, 'src/assets/scss')] },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 });
